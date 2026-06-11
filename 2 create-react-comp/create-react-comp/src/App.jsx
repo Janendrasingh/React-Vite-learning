03a39a2fa5d92c4e1fa641300c144ad754f946da
JANENDRA SINGH <singhjanendra120202@gmail.com>
2026-06-11 13:52:42 +0530

    Reusable Components

--- 2 create-react-comp/create-react-comp/src/App.jsx
+++ 2 create-react-comp/create-react-comp/src/App.jsx
@@ -1,10 +1,16 @@
 import Hello from "./hello";
+import Random from "./Random";
 function App() {
   return <div>
     <h1>
     this is the best react app
   </h1>
-  <Hello />
+  <Hello /> 
+  <Random></Random>
+  <Random></Random>
+  <Random></Random>
+  <Random></Random>
+  <Random></Random>
   </div>
 }
 
---
+++ 2 create-react-comp/create-react-comp/src/Random.jsx
@@ -0,0 +1,7 @@
+function Random(){
+  let number = Math.floor(Math.random() * 10);
+
+    return <h1 style={{background: "#258264"}}>Random number is: {number}</h1>
+}
+
+export default Random;
