c9a23f78c3f1456d99a5b7d5b1a588df417af513
JANENDRA SINGH <singhjanendra120202@gmail.com>
2026-06-14 13:23:50 +0530

    modifided version 0.0.2

--- Projects/1 todo_list/src/App.jsx
+++ Projects/1 todo_list/src/App.jsx
@@ -1,18 +1,22 @@
 import "./App.css";
 import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
-import TodoItem1 from "./components/TodoItem1";
-import TodoItem2 from "./components/TodoItem2";
+import TodoItems from "./components/TodoItems";
 
 function App() {
+
+const todoitems = [
+    { name: 'Buy milk', dueDate: '12/07/26' },
+    { name: 'Collage', dueDate: '13/07/26' },
+    { name: 'Like', dueDate: '14/07/26' },
+
+]
+
   return (
     <center className="todo-container">
       <AppName />
       <AddTodo />
-      <div className="items-container">
-        <TodoItem1 />
-        <TodoItem2 />
-      </div>
+      <TodoItems todoItems={todoitems}/>
     </center>
   );
 }
--- Projects/1 todo_list/src/components/AppName.jsx
+++ Projects/1 todo_list/src/components/AppName.jsx
@@ -1,6 +1,7 @@
+import Ap from "./AppName.module.css";
 function AppName() {
   return (
-    <h1>
+    <h1 className="Ap.todoHeading">
       <b>TODO APP</b>
     </h1>
   );
