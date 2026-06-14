3c6f7ed11e66af9d155a69fd29bd28ff8f325ee9
JANENDRA SINGH <singhjanendra120202@gmail.com>
2026-06-15 00:39:12 +0530

    added the css module function in button

--- Projects/3 Calculator/src/App.jsx
+++ Projects/3 Calculator/src/App.jsx
@@ -1,17 +1,13 @@
 import sty from './App.module.css';
+import ButtonContainer from './buttonsContainer';
+import Display from './components/Display';
 
 function App() {
   return (
     <>
     <div className= {sty.calculator} >
-      <input className={sty.display} type='text' />
-      <div className={sty.buttonContainer}>
-        <button className={sty.button}>C</button>
-        <button className={sty.button}>1</button>
-        <button className={sty.button}>2</button>
-        <button className={sty.button}>3</button>
-        <button className={sty.button}>4</button>
-      </div>
+      <Display />
+      <ButtonContainer />
     </div>
     </>
   )
---
+++ Projects/3 Calculator/src/components/Display.jsx
@@ -0,0 +1,7 @@
+import sty from "./Display.module.css"
+
+const Display=() => {
+    return       <input className={sty.display} type='text' />
+}
+
+export default Display;
