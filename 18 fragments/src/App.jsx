82434c9bb13fbf1240be5b1be002c3ffa452ec07
JANENDRA SINGH <singhjanendra120202@gmail.com>
2026-06-15 14:42:48 +0530

    added states

--- 18 fragments/src/App.jsx
+++ 18 fragments/src/App.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import FoodItems from './components/FoodItems';
-import FoodInput from './components/FoodInput';
-import ErrorMessage from './components/ErrorMessage';
-import Container from './components/Container';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FoodItems from "./components/FoodItems";
+import FoodInput from "./components/FoodInput";
+import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 
 function App() {
-  let foodItems = ["sabji", "Vegitable", "Fruits", "Roti","Milk", "Ghee"]
+  let foodItems = ["sabji", "Vegitable", "Fruits", "Roti", "Milk", "Ghee"];
   //let foodItems = []
-  
-  return (  
-  <>     {/* <----- React Fragment also can we used <React.Fragment></React.Fragment>   */}     
-  <Container>
-  <h1>Healthy food</h1>
-  <ErrorMessage items={foodItems}></ErrorMessage>
-  <FoodInput><input type="text" /></FoodInput>
-  <FoodItems items={foodItems}></FoodItems>
-  </Container>            
-  {/* Map Method */}
 
-  {/* <ul className="list-group">{foodItems.map((item) =>          
+  return (
+    <>
+      {" "}
+      {/* <----- React Fragment also can we used <React.Fragment></React.Fragment>   */}
+      <Container>
+        <h1>Healthy food</h1>
+        <ErrorMessage items={foodItems}></ErrorMessage>
+        <FoodInput>
+          <input type="text" />
+        </FoodInput>
+        <FoodItems items={foodItems}></FoodItems>
+      </Container>
+      {/* Map Method */}
+      {/* <ul className="list-group">{foodItems.map((item) =>          
   <li className="list-group-item">{item}</li>)}
 </ul> */}
-</>)
-    
+    </>
+  );
 }
 
-export default App
+export default App;
