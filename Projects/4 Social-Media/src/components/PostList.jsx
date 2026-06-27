527ce3a53f5be56d25f65db11b8c2877e6346600
JANENDRA SINGH <singhjanendra120202@gmail.com>
2026-06-27 13:40:08 +0530

    applied dummy api

--- Projects/4 Social-Media/src/components/PostList.jsx
+++ Projects/4 Social-Media/src/components/PostList.jsx
@@ -1,23 +1,25 @@
 import { PostListContext } from "../store/post-list-store";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Post from "./Post";
 import Welcome from "./Welcome";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListContext);
+  const [dataFetched, setDataFetched] = useState(false);
 
-  const handleGetPostClick = () => {
+  if(!dataFetched) {
     // Logic to fetch posts from the server can be added here
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
       });
+      setDataFetched(true);
   };
 
   return (
     <>
-      {postList.length === 0 && <Welcome onGetPostClick={handleGetPostClick} />}
+      {postList.length === 0 && <Welcome />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- Projects/4 Social-Media/src/components/Welcome.jsx
+++ Projects/4 Social-Media/src/components/Welcome.jsx
@@ -2,13 +2,6 @@ const Welcome = ({ onGetPostClick }) => {
   return (
     <center className="wel">
       <h1>Welcome to the Social Media App</h1>
-      <button
-        type="button"
-        onClick={onGetPostClick}
-        className="btn btn-primary"
-      >
-        Get Posts From server
-      </button>
     </center>
   );
 };
