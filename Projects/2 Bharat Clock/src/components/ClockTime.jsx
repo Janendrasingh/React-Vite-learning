2055ff08c90c1276af942391ecd9ce9bd8eefc58
JANENDRA SINGH <singhjanendra120202@gmail.com>
2026-07-03 01:27:29 +0530

    useEffect hook Cleanup

--- Projects/2 Bharat Clock/src/components/ClockTime.jsx
+++ Projects/2 Bharat Clock/src/components/ClockTime.jsx
@@ -1,7 +1,24 @@
-function ClockTime(){
-    let time = new Date();
+import { useState, useEffect } from "react";
 
-    return <p  className="lead">This is the current time: {time.toLocaleDateString()} - {" "}{time.toLocaleTimeString()}</p>
+function ClockTime() {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <p className="lead">
+            This is the current time: {time.toLocaleDateString()} -{" "}
+            {time.toLocaleTimeString()}
+        </p>
+  );
 }
 
 export default ClockTime;
